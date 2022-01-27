af9616fda4303788607c709a5dfe53603119525f
Daniela Monteiro <danimdf360@gmail.com>
2022-01-27 19:26:44 -0300

    final project

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -22,8 +20,36 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev DaniDev = new Dev();
+        DaniDev.setNome("Daniela");
+        DaniDev.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Daniela: " + DaniDev.getConteudoInscritos());
+        DaniDev.progredir();
+        System.out.println("--------------");
+        System.out.println("Conteudos Inscritos Daniela: " + DaniDev.getConteudoInscritos());
+        System.out.println("Conteudos Concluidos Daniela: " + DaniDev.getConteudoConcluidos());
+        System.out.println("xp: " + DaniDev.calcularTotalXp());
+
+        System.out.println("--------------");
+
+        Dev OutroDev = new Dev();
+        OutroDev.setNome("Alex");
+        OutroDev.inscreverBootcamp(bootcamp);
+        OutroDev.progredir();
+        System.out.println("--------------");
+        System.out.println("Conteudos Inscritos Outro dev: " + OutroDev.getConteudoInscritos());
+        System.out.println("Conteudos Concluidos Outro dev: " + OutroDev.getConteudoConcluidos());
+        System.out.println("xp: " + OutroDev.calcularTotalXp());
     }
 }
